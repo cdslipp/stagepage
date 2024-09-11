474db9d16d4b7dda1e6b3136a9a1e628bb811cde
Cameron Slipp <cdslipp@gmail.com>
2024-09-11 11:18:09 -0400

    Removing svelte swipe

--- stagepage-frontend/src/lib/components/Credits.svelte
+++ stagepage-frontend/src/lib/components/Credits.svelte
@@ -1,5 +1,5 @@
 <script>
-	let credits = [
+	export let credits = [
 		{ role: 'Written & Performed by', name: 'Ahmad Meree' },
 		{ role: 'Directed by', name: 'Majdi Bou-Matar' },
 		{ role: 'Set Design', name: 'Majdi Bou-Matar' },
@@ -12,30 +12,41 @@
 	];
 </script>
 
-<article>
-	<section class="credits">
-		<h2>Credits</h2>
-		<ul>
-			{#each credits as credit}
-				<li>
-					<span class="role">{credit.role}:</span>
-					<span class="name">{credit.name}</span>
-				</li>
-			{/each}
-		</ul>
-	</section>
-</article>
+<section class="credits">
+	<ul>
+		{#each credits as credit}
+			<li>
+				<span class="role">{credit.role}</span>
+				<span class="name">{credit.name}</span>
+			</li>
+		{/each}
+	</ul>
+</section>
 
 <style>
+	.credits {
+		margin: 40px 0;
+	}
+	.credits h2 {
+		font-size: 24px;
+		margin-bottom: 20px;
+	}
 	.credits ul {
 		list-style-type: none;
 		padding: 0;
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+		gap: 20px;
 	}
 	.credits li {
-		margin-bottom: 10px;
+		display: flex;
+		flex-direction: column;
 	}
 	.role {
-		font-weight: bold;
-		margin-right: 5px;
+		font-weight: 700;
+		margin-bottom: 5px;
+	}
+	.name {
+		font-size: 18px;
 	}
 </style>
